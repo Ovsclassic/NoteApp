112afe0ec025af0dc9b89aadd1add412a259a99a
Ovsclassic <okunoyevictor@gmail.com>
2023-03-27 21:18:43 +0100

    Created functions for editing the note page

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,21 +1,55 @@
 import { BiSearch } from "react-icons/Bi";
+import { MdClose } from "react-icons/Md";
 import { Link } from "react-router-dom";
 import { BsPlusLg } from "react-icons/bs";
 
 import NoteItem from "../components/NoteItem";
+import { useEffect, useState } from "react";
 
 const Notes = ({ notes }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes); //for the notes stored on the localstorage
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      notes.filter((note) => {
+        if (note.title.toLowerCase().match(text.toLocaleLowerCase())) {
+          return note;
+        }
+      })
+    );
+  };
+  useEffect(handleSearch, [text]);
+
   return (
     <section>
       <header className="notes__header">
-        <h2>My Notes</h2>
-        {/*<input type="text" autoFocus placeholder="Keyword..." />*/}
-        <button className="btn">
-          <BiSearch />
+        {!showSearch && <h2>My Notes</h2>}
+        {showSearch && (
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              handleSearch();
+            }}
+            autoFocus
+            placeholder="Keyword..."
+          />
+        )}
+        <button
+          className="btn"
+          onClick={() => setShowSearch((prevState) => !prevState)}
+        >
+          {showSearch ? <MdClose /> : <BiSearch />}
         </button>
       </header>
       <div className="notes__container">
-        {notes.map((note) => (
+        {filteredNotes.length == 0 && (
+          <p className="empty__notes">No Notes Found.</p>
+        )}
+        {filteredNotes.map((note) => (
           <NoteItem key={note.id} note={note} />
         ))}
       </div>
