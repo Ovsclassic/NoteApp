ba2c88a9750fc85a27d9c2a32b60d8ad8166a2c6
Ovsclassic <okunoyevictor@gmail.com>
2023-03-27 15:39:14 +0100

    Added  localstorage &feature for creating newnote

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { useState } from "react";
 
-const EditNote = () => {
+const EditNote = ({ notes, setNotes }) => {
+  const { id } = useParams();
+  const note = notes.find((item) => item.id == id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+
   return (
     <section>
       <header className="create-note__header">
@@ -13,8 +19,19 @@ const EditNote = () => {
         <button className="btn danger">RiDeleteBin6Line</button>
       </header>
       <form className="create-note__form">
-        <input type="text" placeholder="Title" autoFocus />
-        <textarea rows="28" placeholder="Note details..."></textarea>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+        />
+        <textarea
+          rows="28"
+          placeholder="Note details..."
+          value={details}
+          onChange={(e) => setTitle(e.target.value)}
+        ></textarea>
       </form>
     </section>
   );
